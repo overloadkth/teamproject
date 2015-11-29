fcdc6c83e4fbbf5bcf0d02376d7504644eb9a009
narsimplyme <narsimplyme@naver.com>
2015-11-30 02:23:11 +0900

    MyiWeb nav list
    todo: drag to hide/show

--- app/src/main/java/com/example/myapp/MyiWeb.java
+++ app/src/main/java/com/example/myapp/MyiWeb.java
@@ -2,16 +2,15 @@ package com.example.myapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.webkit.CookieManager;
-import android.webkit.CookieSyncManager;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import static com.example.myapp.R.layout.activity_myiweb;
 
@@ -19,9 +18,15 @@ import static com.example.myapp.R.layout.activity_myiweb;
  * Created by MeromGreen on 2015-11-24.
  */
 public class MyiWeb extends Activity{
-    WebView webview;
-    String email;
-    String password;
+    private WebView hiddenWebView;
+    private WebView frontWebView;
+    private String email;
+    private String password;
+
+    private String[] navItems = {"성적조회", "학생카드"};
+    private ListView lvNavList;
+    private RelativeLayout flContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,48 +36,62 @@ public class MyiWeb extends Activity{
         email = bundle.getString("ID");
         password = bundle.getString("PW");
 
-        webview = (WebView)findViewById(R.id.logIn);
-        webview.clearCache(true);
-        webview.setWebViewClient(new WebClient() {
+        hiddenWebView = (WebView)findViewById(R.id.hiddenWV);
+        hiddenWebView.clearCache(true);
+        hiddenWebView.loadUrl("http://sso.mju.ac.kr/swift/login/login_myiweb.jsp");
+        hiddenWebView.getSettings().setDatabaseEnabled(true);
+        hiddenWebView.getSettings().setJavaScriptEnabled(true);
+
+
+        hiddenWebView.setWebViewClient(new WebClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                webview.loadUrl("javascript: {" +
+                hiddenWebView.loadUrl("javascript: {" +
                         "document.getElementById('userID').value = '" + email + "';" +
                         "document.getElementById('userPW').value = '" + password + "';" +
-                        "var a = document.getElementsByTagName('input');" +
-                        "a[2].CheckSubmit(); };");
-                webview.loadUrl("javascript:CheckSubmit()");
+                        "var a = document.getElementsByTagName('input');};");
+                hiddenWebView.loadUrl("javascript:CheckSubmit()");
             }
         });
-        webview.getSettings().setDatabaseEnabled(true);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("http://sso.mju.ac.kr/swift/login/login_myiweb.jsp");
 
-        //login(email, password);
+        frontWebView = (WebView)findViewById(R.id.frontWV);
+        frontWebView.clearCache(true);
+        frontWebView.getSettings().setDatabaseEnabled(true);
+        frontWebView.getSettings().setJavaScriptEnabled(true);
+        frontWebView.setWebViewClient(new WebClient());
 
+
+        lvNavList = (ListView) findViewById(R.id.lv_activity_main_nav_list);
+        flContainer = (RelativeLayout)findViewById(R.id.fl_activity_main_container);
+
+        lvNavList.setAdapter(
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navItems));
+        lvNavList.setOnItemClickListener(new DrawerItemClickListener());
 
 
     }
 
-
-    private void login(String id, String pw){
-        webview.loadUrl("javascript: {" +
-                "document.getElementById('userID').value = '"+email +"';" +
-                "document.getElementById('userPW').value = '"+password+"';" +
-                "var a = document.getElementsByTagName('input');" +
-                "a[2].CheckSubmit(); };");
-        webview.loadUrl("javascript:CheckSubmit()");
-//        webview.loadUrl("javascript: {" +
-//                "var useless = document.getElementById('userID').value = '60122';" +
-//                "var useles2s = document.getElementById('userPW').value = '3233222';" +
-//                "var a = document.getElementsByTagName('input');" +
-//                "a[2].CheckSubmit(); };");
-//        webview.loadUrl("javascript:CheckSubmit()");
-    }
     class WebClient extends WebViewClient {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+    }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapter, View view, int position,
+                                long id) {
+            switch (position) {
+                case 0:
+                    hiddenWebView.loadUrl("https://myiweb.mju.ac.kr/servlet/su.suh.suh03.Suh03Svl02?attribute=getGrade&studentCd=60122469");
+                    break;
+                case 1:
+                    hiddenWebView.loadUrl("https://myiweb.mju.ac.kr/su/sum/sum01/w_sum010Main.jsp");
+                    break;
+
+            }
+
         }
     }
 }
